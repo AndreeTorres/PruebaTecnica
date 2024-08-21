522c778ee0b3d62effec38fbc4883baf0d965acf
Andree Torres <andreetorres@MacBook-Pro-de-Andree.local>
2024-08-21 00:09:36 -0600

    FIX:MIDDLEWARES

--- PruebaTecnica/PruebaTecnica/settings.py
+++ PruebaTecnica/PruebaTecnica/settings.py
@@ -127,6 +127,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',  
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  
+    'django.contrib.messages.middleware.MessageMiddleware',  
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
