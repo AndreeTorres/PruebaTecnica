efa345805ceb040392cae3791f37bd964ce65a51
Andree Torres <andreetorres@MacBook-Pro-de-Andree.local>
2024-08-20 18:55:31 -0600

    ADD:Models and Templates :construction:

--- PruebaTecnica/parkingApp/views.py
+++ PruebaTecnica/parkingApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Parking
 
 # Create your views here.
+def parking_view(request):
+    parking = Parking.objects.first()
+    context = {
+        'total_spaces': parking.total_spaces,
+        'available_spaces': parking.available_spaces,
+    }
+    return render(request, 'parking.html', context)
